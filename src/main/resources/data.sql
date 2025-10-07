55c0129e220f23258f8ca924b12fcafa2ce105d1
DANIEL GUSTAVO DAVILA VELASQUEZ <danieldavila1803@gmail.com>
2025-10-07 00:27:40 -0500

    Modificar archivo data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO products (nombre, precio, stock) VALUES ('CPU', 5000.00, 10);
-INSERT INTO products (nombre, precio, stock) VALUES ('Monitor', 1500.00, 10);
-INSERT INTO products (nombre, precio, stock) VALUES ('Teclado', 250.00, 15);
-INSERT INTO products (nombre, precio, stock) VALUES ('Mouse', 100.00, 5);
-INSERT INTO products (nombre, precio, stock) VALUES ('Impresora', 700.00, 2);
+INSERT INTO products (id, nombre, precio, stock) VALUES (1, 'CPU', 5000.00, 10);
+INSERT INTO products (id, nombre, precio, stock) VALUES (2, 'Monitor', 1500.00, 10);
+INSERT INTO products (id, nombre, precio, stock) VALUES (3, 'Teclado', 250.00, 15);
+INSERT INTO products (id, nombre, precio, stock) VALUES (4, 'Mouse', 100.00, 5);
+INSERT INTO products (id, nombre, precio, stock) VALUES (5, 'Impresora', 700.00, 2);
